f1f50fa3f29f208a6eaeebc318bf75486179100a
Certifieddonnie <mcdonaldsamure91@gmail.com>
2023-08-26 20:09:48 +0100

    [feat]: database architecture

---
+++ Hostel_Management/database.py
@@ -0,0 +1,53 @@
+""" Database Architecture """
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+import os
+from dotenv import load_dotenv, find_dotenv
+
+
+load_dotenv(find_dotenv())
+
+MYSQL_USER = os.getenv("MYSQL_USER")
+MYSQL_PWD = os.getenv("MYSQL_PWD")
+MYSQL_HOST = os.getenv("MYSQL_HOST")
+MYSQL_DB = os.getenv("MYSQL_DB")
+
+
+# Create a database engine and session
+engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
+                       format(MYSQL_USER,
+                              MYSQL_PWD,
+                              MYSQL_HOST,
+                              MYSQL_DB))
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# Create a base class for declarative models
+Base = declarative_base()
+
+# Define your database model classes
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    first_name = Column(String, nullable=False)
+    last_name = Column(String, nullable=False)
+    user_name = Column(String, nullable=False)
+    password = Column(String, nullable=False)
+    user_type = Column(String, nullable=False)
+    gender = Column(String, nullable=False)
+
+
+class Student(Base):
+    __tablename__ = 'students'
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String, nullable=False)
+    hostel_id = Column(Integer, nullable=False)
+
+
+# Create the database tables (only needed once)
+Base.metadata.create_all(engine)
